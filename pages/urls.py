2b1a99f75b6712bb0fb6cfcdfc7976e90c524ea7
M4YH3M <tengodzebisashvili6@gmail.com>
2025-11-11 15:44:38 +0400

    asdasd

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomePageView
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-   
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
  ]
